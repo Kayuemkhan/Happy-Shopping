03456bca545f5556150e013ce6242762f2e9e6a0
Kayuemkhan <abdulkayuem007@gmail.com>
2021-02-02 11:08:06 +0600

    Created commit in 2-2-2021

---
+++ app/src/main/java/code/fortomorrow/riceapp/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package code.fortomorrow.riceapp.Prevalent;
+
+import code.fortomorrow.riceapp.Model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
---
+++ app/src/main/java/code/fortomorrow/riceapp/SettinsActivity.java
@@ -0,0 +1,14 @@
+package code.fortomorrow.riceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettinsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settins);
+    }
+}
